7d2975afda17312297ac7ddb53f8e6b62f259e6e
Devina <44644511+Devina6@users.noreply.github.com>
2023-06-30 12:48:28 +0530

    Create admin controller

---
+++ controllers/admin.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const rootDir = require('../util/path');
+const User = require('../models/users');
+
+exports.getAddUser = (req,res,next) => {
+    console.log('admin controller');
+}
+
