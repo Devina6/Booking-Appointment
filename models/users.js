7810da6a8d26b9185546abfa515da85a8c41ba83
Devina <44644511+Devina6@users.noreply.github.com>
2023-06-30 12:45:58 +0530

    Update users.js

--- models/users.js
+++ models/users.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 const sequelize = require('../util/database');
 
-const User = squelize.define('user',{
+const User = sequelize.define('user',{
     id:{
         type:Sequelize.INTEGER,
         autoIncrement:true,
