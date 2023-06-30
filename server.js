130609e45e6dd3dcadfc86fd6ae31711000630b2
Devina <44644511+Devina6@users.noreply.github.com>
2023-06-30 12:55:55 +0530

    404 controller

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const sequelize = require('./util/database');
 const bodyParser = require('body-parser');
+const controller404 = require('./controllers/404');
 
 app.use(bodyParser.urlencoded({extended:false}));
 
@@ -11,6 +12,8 @@ const userRoutes = require('./routes/user');
 
 app.use(userRoutes);
 app.use('/admin',adminRoutes);
+
+app.use(controller404.get404);
 
 sequelize.sync()
     .then(result =>{
