62b75b3a885dec03ca41795946c6860207564d05
Devina <44644511+Devina6@users.noreply.github.com>
2023-06-30 12:45:24 +0530

    add user and admin routes

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended:false}));
 
+const adminRoutes = require('./routes/admin');
+const userRoutes = require('./routes/user');
+
+app.use(userRoutes);
+app.use('/admin',adminRoutes);
+
 sequelize.sync()
     .then(result =>{
         app.listen(5000);
