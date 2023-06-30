8ec22ec33c76818c5b2afc73cfe7206b3c185f3e
Devina <44644511+Devina6@users.noreply.github.com>
2023-06-30 18:31:57 +0530

    getUsers, getUser, deleteUser routes

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user');
 
-router.get('/',userController.getUserIndex);
+router.get('/',userController.getIndex);
+router.get('/users',userController.getUsers);
+router.get('/user/:id',userController.getUser);
+router.get('/delete/:id',userController.deleteUser);
 
 module.exports = router;
