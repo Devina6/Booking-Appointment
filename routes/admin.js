beeaa651b5927ab63ef137cfc7c133d17c5e1046
Devina <44644511+Devina6@users.noreply.github.com>
2023-06-30 18:30:42 +0530

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const adminController = require('../controllers/admin');
 
-router.get('/add-user',adminController.getAddUser);
+router.post('/add-user',adminController.postAddUser);
 
 module.exports = router;
