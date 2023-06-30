03a438e333c6047fb1ae85d57a7a3b1cd2b840f6
Devina <44644511+Devina6@users.noreply.github.com>
2023-06-30 12:18:58 +0530

    connect sequelize

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const sequelize = require('./util/database');
+const bodyParser = require('body-parser');
 
-app.listen(5000);
+app.use(bodyParser.urlencoded({extended:false}));
+
+sequelize.sync()
+    .then(result =>{
+        app.listen(5000);
+    })
+    .catch(err => console.log(err))
