c876456896b9ef8dae83664fbf2c23372002fe54
Devina <44644511+Devina6@users.noreply.github.com>
2023-06-30 12:48:55 +0530

    Create user controller

---
+++ controllers/user.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const rootDir = require('../util/path');
+const User = require('../models/users');
+const fs = require('fs');
+
+exports.getUserIndex = (req,res,next) => {
+    console.log('user control');
+}
+
