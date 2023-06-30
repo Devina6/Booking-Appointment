4cd1985017e4c6c19c29558a34b9383ddb0ddde3
Devina <44644511+Devina6@users.noreply.github.com>
2023-06-30 18:28:28 +0530

    post user details in database

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,8 +1,19 @@
 const path = require('path');
 const rootDir = require('../util/path');
 const User = require('../models/users');
+const fs = require('fs');
 
-exports.getAddUser = (req,res,next) => {
-    console.log('admin controller');
+exports.postAddUser = (req,res,next) => {
+    const name = req.body.name;
+    const dob = req.body.dob;
+    const mobile = req.body.mobile;
+    const email = req.body.email;
+    User.create({
+        name:name,
+        dob:dob,
+        mobile:mobile,
+        email:email
+    }).then(result => {
+        return res.redirect('/users');
+    }).catch(err => console.log(err))
 }
-
